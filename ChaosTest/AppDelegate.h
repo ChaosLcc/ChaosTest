4af762eaca4cc9df180d1024116700c1fd8a4d4d
LiuBi_GC_iMac <liubi_gc_imac@LiuBi-GC-iMacs-iMac.local>
2018-06-05 08:48:42 +0800

    初始化项目

---
+++ ChaosTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChaosTest
+//
+//  Created by  LiuBi_GC_iMac on 2018/6/5.
+//  Copyright © 2018年 cn.sixarm.seller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChaosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChaosTest
+//
+//  Created by  LiuBi_GC_iMac on 2018/6/5.
+//  Copyright © 2018年 cn.sixarm.seller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
